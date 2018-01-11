ad4d1ce4a6b94f5513c2d7d8b895f4f9f2c61d7b
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-01-11 16:53:52 +0530

    Create present_in_bet.c

---
+++ present_in_bet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n,l,r;
+printf("Enter n:");
+scanf("%d",&n);
+printf("\n Enter l and r:");
+scanf("%d%d",&l,&r);
+if(l<n<r)
+printf("Present");
+else
+printf("Not present");
+}
