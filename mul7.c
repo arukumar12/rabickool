71e9830c02cd5b272bc1290cbf82dd6385bf5bf9
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-01-18 16:07:56 +0530

    Update mul7.c

--- mul7.c
+++ mul7.c
@@ -4,7 +4,7 @@ void main()
   int a;
   scanf("%d",&a);
   if(a%7==0)
-  printf("\Multiple of 7");
+  printf("\n Multiple of 7");
   else
-  printf("\Not Multiple of 7");  
+  printf("\n Not Multiple of 7");  
 }
