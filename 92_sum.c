5a20f661e7ee594289a25be8a092d627c5fb6fa9
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-01-19 08:33:54 +0000

    Create 92_sum.c

---
+++ 92_sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[100],n,i,sum;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+printf("Sum is:%d",sum);
+}
