d5593701032fe89fb22a65e4a19c88d47e9ce41a
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-01-25 16:02:33 +0530

    Create 97_reverse.c

---
+++ 97_reverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,rev=0,r;
+printf("Enter the Number:");
+scanf("%d",&a);
+while(a!=0)
+{
+r=a%10;
+rev=rev*10+r;
+a/=10;
+}
+printf("The reverse of the %d is %d",a,rev);
+}
