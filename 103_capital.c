4adc0b052da2e9c5f2bb74245f8029bd6d12f56a
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-02-01 08:07:05 +0000

    Update 103_capital.c

--- 103_capital.c
+++ 103_capital.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+void main()
+{
+    char str[100],i;
+    printf("Enter the string:");
+    scanf("%[^\n]s",str);
+    str[0]=toupper(str[0]);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+        {
+            str[i+1]=toupper(str[i+1]);
+        }
+    }
+    printf("%s",&str);
+}
